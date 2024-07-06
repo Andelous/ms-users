68612ec4fec5f593acf75499b416b70b476fa712
Angel Daniel Alonso Tovar <andelous.aa@gmail.com>
2024-07-05 21:57:59 -0500

    Sync

--- ms-users-client/src/main/java/com/example/ms/users/MSUsersClient.java
+++ ms-users-client/src/main/java/com/example/ms/users/MSUsersClient.java
@@ -1,32 +1,93 @@
 package com.example.ms.users;
 
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Response.StatusType;
 
-import com.example.ms.users.def.MSUsersDefinition;
-import com.example.ms.users.ex.ExistingUserException;
-import com.example.ms.users.ex.IncorrectPasswordException;
-import com.example.ms.users.ex.IncorrectUsernameException;
-import com.example.ms.users.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class MSUsersClient implements MSUsersDefinition {
+import com.example.ms.users.def.MSUsers;
+import com.example.ms.users.def.ex.ExistingUserException;
+import com.example.ms.users.def.ex.IncorrectPasswordException;
+import com.example.ms.users.def.ex.IncorrectUsernameException;
+import com.example.ms.users.def.model.User;
+
+public class MSUsersClient implements MSUsers {
+	private static final Logger LOGGER = LoggerFactory.getLogger(MSUsersClient.class);
+
+	private final Client client;
+	private final WebTarget targetBase;
+	private final WebTarget targetAddUser;
+	private final WebTarget targetAuthenticateUser;
+
+	public MSUsersClient(String baseURI) {
+		client = ClientBuilder.newClient();
+		
+		client.register(JSO)
+		
+		targetBase = client.target(baseURI);
+		targetAddUser = targetBase.path("users");
+		targetAuthenticateUser = targetBase.path("users").path("auth");
+	}
 
 	@Override
 	public void addUser(User user) throws ExistingUserException {
-		// TODO Auto-generated method stub
-		ClientBuilder.newClient();
+		Response response = targetAddUser.request(MediaType.TEXT_PLAIN)
+				.post(Entity.entity(user, MediaType.APPLICATION_JSON));
+
+		StatusType status = response.getStatusInfo();
+		String exceptionType = response.getHeaderString("Exception-Type");
+		String responseBody = response.readEntity(String.class);
+
+		boolean isSuccess = status == Status.ACCEPTED || status == Status.OK || status == Status.NO_CONTENT;
+		boolean isClientError = status == Status.BAD_REQUEST;
+		boolean isServerError = !isSuccess && !isClientError;
+
+		response.close();
+
+		if (isSuccess)
+			return;
+
+		if (isClientError)
+			switch (exceptionType) {
+
+			case "ExistingUserException":
+				throw new ExistingUserException(user.getUsername(), responseBody, null);
+
+			default:
+				throw new RuntimeException("UNRECOGNIZED EXCEPTION FROM SERVER [" + exceptionType + "]. Status code ["
+						+ status + "] - Message from server: " + responseBody);
+
+			}
+
+		if (isServerError)
+			throw new RuntimeException(
+					"Error while calling server. Status code [" + status + "] - Message from server: " + responseBody);
 	}
 
 	@Override
 	public String authenticateUser(String username, String password)
 			throws IncorrectPasswordException, IncorrectUsernameException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public User getUserByToken(String token) {
-		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void main(String[] args) throws ExistingUserException {
+		MSUsersClient client = new MSUsersClient("http://localhost:8080/ms-users-service/");
+
+		User user = new User("andelous", "mexico2005");
+
+		client.addUser(user);
 	}
 
 }
